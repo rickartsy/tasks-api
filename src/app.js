5442c85524142ac41efbc8ceb87a909640c2c61d
rickartsy <syafaa.edu@gmail.com>
2026-03-03 13:47:15 +0700

    feat: implement create task endpoint

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 const tasks = [
     { id: 1, title: "Learn Express", completed: false },
     { id: 2, title: "Study REST API", completed: false }
@@ -11,6 +13,25 @@ const tasks = [
 // GET /tasks
 app.get('/tasks', (req, res) => {
     res.json(tasks);
+});
+
+// POST /tasks
+app.post('/tasks', (req, res) => {
+    const { title } = req.body;
+
+    if (!title) {
+        return res.status(400).json({ message: 'Title is required'});
+    }
+
+    const newTask = {
+        id: tasks.length + 1,
+        title,
+        completed: false
+    };
+
+    tasks.push(newTask);
+
+    return res.status(201).json(newTask);
 });
 
 app.listen(port, () => {
