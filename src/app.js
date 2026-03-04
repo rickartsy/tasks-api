323015df10e9aa6d90d61c77cd425682287cc40b
rickartsy <syafaa.edu@gmail.com>
2026-03-04 13:50:14 +0700

    feat: add centralized error handling middleware

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const port = 3000;
 
 import taskRoutes from './routes/taskRoutes.js';
 import { logger } from './middleware/logger.js';
+import { errorHandler } from './middleware/errorHandler.js';
 
 app.use(logger);
 app.use(express.json());
@@ -12,6 +13,7 @@ app.use('/tasks', taskRoutes);
 app.use((req, res) => {
     res.status(404).json({ message: 'Route not found' });
 });
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
