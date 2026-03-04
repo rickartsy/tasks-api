4c665cc78fec5697b638917afe310c61db4c6255
rickartsy <syafaa.edu@gmail.com>
2026-03-04 13:23:52 +0700

    feat: implement 404 not found handler

--- src/app.js
+++ src/app.js
@@ -6,9 +6,12 @@ const port = 3000;
 import taskRoutes from './routes/taskRoutes.js';
 import { logger } from './middleware/logger.js';
 
-app.use(express.json());
 app.use(logger);
+app.use(express.json());
 app.use('/tasks', taskRoutes);
+app.use((req, res) => {
+    res.status(404).json({ message: 'Route not found' });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
